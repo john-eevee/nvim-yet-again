375878838151b5a02b79f0e1b61f7935923b0dfe
John <5256711+john-eevee@users.noreply.github.com>
2026-02-01 23:16:52 -0300

    feat: disable lazyvim startup screen (alpha/dashboard)

---
+++ lua/plugins/ui-disable-dashboard.lua
@@ -0,0 +1,17 @@
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+-- Disable LazyVim Startup Screen
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+-- This file disables the alpha/dashboard startup screen from LazyVim
+-- Start Neovim directly with the last file or empty buffer
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+return {
+  {
+    "goolord/alpha-nvim",
+    enabled = false,
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    enabled = false,
+  },
+}
