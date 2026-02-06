373b7c379ca4618de4c89813f4f0349ba3bb6fd9
John <5256711+john-eevee@users.noreply.github.com>
2026-02-06 00:27:59 -0300

    Configure blink.cmp to accept on Enter and cycle with Tab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -15,7 +15,18 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = { preset = "default" },
+      keymap = {
+        preset = "none",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+      },
 
       appearance = {
         use_nvim_cmp_as_default = true,
