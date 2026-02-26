9ae71ea73e466ed1e3c0a3f6f2b3afa56792aa11
John <5256711+john-eevee@users.noreply.github.com>
2026-02-26 16:13:25 -0300

    Add diamond operator <> to mini.pairs for Java

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,5 +1,11 @@
 return {
-  { "echasnovski/mini.pairs", version = "*", config = function() require("mini.pairs").setup() end },
+  { "echasnovski/mini.pairs", version = "*", config = function()
+    require("mini.pairs").setup({
+      mappings = {
+        ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\]' },
+      },
+    })
+  end },
   { "echasnovski/mini.comment", version = "*", config = function() require("mini.comment").setup() end },
   { "echasnovski/mini.ai", version = "*", config = function() require("mini.ai").setup() end },
 }
