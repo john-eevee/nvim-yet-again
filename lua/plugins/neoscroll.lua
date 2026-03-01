0d108ab092bb5911fb4beafde06630008142c6d8
John <5256711+john-eevee@users.noreply.github.com>
2026-03-01 03:03:07 -0300

    add neoscroll and smear

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,21 @@
+return {
+  "karb94/neoscroll.nvim",
+  event = "WinScrolled",
+  opts = {
+    mappings = {
+      "<C-u>",
+      "<C-d>",
+      "<C-b>",
+      "<C-f>",
+      "<C-y>",
+      "<C-e>",
+      "zt",
+      "zz",
+      "zb",
+    },
+    hide_cursor = true,
+    stop_eof = true,
+    respect_scrolloff = false,
+    easing_function = "quadratic",
+  },
+}
---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,13 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    cursor_color = "#f5e0dc",
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    distance_stop_animating_vertical_bar = 1,
+    hide_target_hack = false,
+  },
+}
