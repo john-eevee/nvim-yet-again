77c1d8a85bc410bfcaa91c25c455a82e27464bf3
John Ev. <5256711+john-eevee@users.noreply.github.com>
2026-04-09 12:20:43 -0300

    idk

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -15,6 +15,9 @@ return {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
       },
+      progress = {
+        enabled = false,
+      },
     },
   },
   dependencies = {
